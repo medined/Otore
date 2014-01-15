a1c0f19f76f791d288e1f325ad8646e60786ca3b
David Medinets <david.medinets@gmail.com>
2014-01-14 22:55:26 -0500

    Update packages. Setup supergroup (used for hadoop). Install some packages.

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -6,3 +6,26 @@ host { 'affy-slave1.example.com': ip => '10.211.55.101' }
 host { 'affy-slave2.example.com': ip => '10.211.55.102' }
 
 sysctl::value { "vm.swappiness": value => "10"}
+
+exec { "apt-get update":
+    command => "/usr/bin/apt-get -y update"
+}
+
+group { "supergroup":
+    ensure => "present"
+}
+
+user { "root":
+    ensure => present,
+    gid => "supergroup"
+}
+
+user { "vagrant":
+    ensure => present,
+    gid => "supergroup"
+}
+
+package { [ 'curl', 'git', 'maven', 'x11-apps' ] :
+    ensure  => 'installed',
+    require => Exec['apt-get update']
+}
